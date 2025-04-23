ae0eadc756cc5ab354446e01efccf2979eae6f23
Dipali1818 <dipaligavade64@gmail.com>
2025-04-23 17:53:57 +0530

    Add files via upload

---
+++ authController.js
@@ -0,0 +1,10 @@
+// controllers/authController.js
+export const login = (req, res) => {
+  // Implement login logic, e.g., checking credentials
+  res.json({ message: 'Login functionality' });
+};
+
+export const register = (req, res) => {
+  // Implement registration logic
+  res.json({ message: 'Register functionality' });
+};
